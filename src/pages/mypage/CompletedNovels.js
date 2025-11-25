f33b5587ff3ff7354ede2478abaaca9b7d57cd02
0521kimyi <0521kimyi@gmail.com>
2025-11-25 20:15:25 +0900

    fix: 드롭다운버튼 다크모드 적용

--- src/pages/mypage/CompletedNovels.js
+++ src/pages/mypage/CompletedNovels.js
@@ -100,7 +100,10 @@ const Select = styled.select`
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23333' d='M6 9L1 4h10z'/%3E%3C/svg%3E");
+  background-image: ${({ theme }) => {
+    const arrowColor = theme.mode === 'dark' ? '%23ccc' : '%23333';
+    return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='${arrowColor}' d='M6 9L1 4h10z'/%3E%3C/svg%3E")`;
+  }};
   background-repeat: no-repeat;
   background-position: right 12px center;
   background-size: 12px;
