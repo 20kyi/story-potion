5cbb471e7ab84153d33bb12c16c35af505edf508
0521kimyi <0521kimyi@gmail.com>
2025-11-25 20:17:55 +0900

    ""

--- src/pages/mypage/CompletedNovels.js
+++ src/pages/mypage/CompletedNovels.js
@@ -101,9 +101,9 @@ const Select = styled.select`
   -moz-appearance: none;
   appearance: none;
   background-image: ${({ theme }) => {
-    const arrowColor = theme.mode === 'dark' ? '%23ccc' : '%23333';
-    return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='${arrowColor}' d='M6 9L1 4h10z'/%3E%3C/svg%3E")`;
-  }};
+        const arrowColor = theme.mode === 'dark' ? '%23ccc' : '%23333';
+        return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='${arrowColor}' d='M6 9L1 4h10z'/%3E%3C/svg%3E")`;
+    }};
   background-repeat: no-repeat;
   background-position: right 12px center;
   background-size: 12px;
